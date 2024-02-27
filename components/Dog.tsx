f8f0c25154f693dfe65bed4c359086de914a78e0
Josojmf <joso.jmf@gmail.com>
2024-02-27 21:33:10 +0100

    Practica_2 Casi Terminada

--- components/Dog.tsx
+++ components/Dog.tsx
@@ -1,24 +1,23 @@
 import { FunctionComponent } from "https://esm.sh/v128/preact@10.19.2/src/index.js";
 import { type PageProps } from "$fresh/server.ts";
 
-type Dog = {
+type Dog ={
     name: string;
-    latitude: number;
-    longitude: number;
-    country: string;
-    population: number;
-    is_capital: boolean;
-};
+    image_link:string;
+    max_height_male :number;
+    max_height_female :number;
+    max_weight_male :number;
+    max_weight_female :number;
+    good_with_children :number;
+    good_with_dogs :number;
+    good_with_strangers:number;
+    };
 const Dog: FunctionComponent<{dog:Dog}> = (props) => {
     return (
         <div>
-            <h1 className="CityDiv">{props.dog.name}</h1>
+            <h1 className="DogDiv">{props.dog.image_link}</h1>
             <div>
-            <h2>{props.dog.country}</h2>
-            <h3>{props.dog.latitude}</h3>
-            <h4>{props.dog.longitude}</h4>
-            <h5>{props.dog.population}</h5>
-            <h6>{props.dog.is_capital}</h6>
+            <h2>{props.dog.max_height
             /</div>
         </div>
     );
